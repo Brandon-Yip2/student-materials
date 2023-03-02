e6317a07695e18e6e7dece1f10dd10f1d492057f
Brandon-Yip2 <73593739+Brandon-Yip2@users.noreply.github.com>
2023-03-02 14:36:20 -0500

    Alignment + Cohesion + Separation working in student

--- Assignments/Ex1 - Flocking/Source/StudentAI/Flock.cs
+++ Assignments/Ex1 - Flocking/Source/StudentAI/Flock.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Reflection;
 using AI.SteeringBehaviors.Core;
 
 namespace AI.SteeringBehaviors.StudentAI
@@ -17,30 +18,125 @@ namespace AI.SteeringBehaviors.StudentAI
         public virtual void Update(float deltaTime)
         {
             // Update goes here
-            
+
             //First calculate the AverageForward and Average Position
+            calcAverageForward();
+            calcAveragePosition();
+
+            //For each boid, update the separation, alignment, and cohesion
+            foreach (MovingObject m in Boids) {
+
+                Vector3 accel = CalcAlignmentAccel(m);
+                accel += CalcCohesionAccel(m);
+                accel += CalcSeparationAccel(m);
+                float accelMultiplier = m.MaxSpeed;
+                accel *= accelMultiplier * deltaTime;
+                m.Velocity += accel;
+
+                //Calculate Velocities maginitude
+                float velocityMag = m.Velocity.X * m.Velocity.X + m.Velocity.Y * m.Velocity.Y + m.Velocity.Z * m.Velocity.Z;
+
+                if (velocityMag > m.MaxSpeed) {
+
+                    m.Velocity.Normalize();
+                    m.Velocity *= m.MaxSpeed;
+                }
+                m.Update(deltaTime);
+
+            }
+            
 
 
 
         }
         #endregion
 
-        private Vector3 calcAverageForward() {
+        private void calcAverageForward() {
 
-            return 0;
+            float avgX = 0, avgY = 0, avgZ = 0;
+
+            foreach (MovingObject b in Boids)
+            {
+                avgX += b.Velocity.X;
+                avgY += b.Velocity.Y;
+                avgZ += b.Velocity.Z;
+            }
+            avgX /= Boids.Count;
+            avgY /= Boids.Count;
+            avgZ /= Boids.Count;
+
+            Vector3 direction = new Vector3(avgX, avgY, avgZ);
+
+            AverageForward = direction;
+
+
+        }
+
+        private void calcAveragePosition()
+        {
+
+            float avgX=0, avgY=0, avgZ=0;
+
+            foreach (MovingObject b in Boids) {
+                avgX += b.Position.X;
+                avgY += b.Position.Y; 
+                avgZ += b.Position.Z; 
+            }
+            avgX /= Boids.Count;
+            avgY /= Boids.Count;
+            avgZ /= Boids.Count;
+
+            Vector3 position = new Vector3(avgX, avgY, avgZ);
+
+            AveragePosition = position;
+
         }
 
 
-        private Vector3 CalcAlignmentAccel() {
-        
+        private Vector3 CalcAlignmentAccel(MovingObject boid) {
+            Vector3 v = AverageForward / boid.MaxSpeed;
+            float magnitude = v.X * v.X + v.Y * v.Y + v.Z * v.Z;
+            if (magnitude > 1) {
+                v.Normalize();
+            }
+            return v;
         }
 
-        private Vector3 CalcCohesionAccel() { 
-        
+        private Vector3 CalcCohesionAccel(MovingObject boid) {
+            Vector3 sum = new Vector3(0, 0, 0);
+
+            foreach (MovingObject otherB in Boids) {
+                Vector3 vector = boid.Position - otherB.Position;
+                float distance = vector.X * vector.X + vector.Y * vector.Y + vector.Z * vector.Z;
+                float safeDistance = boid.SafeRadius + otherB.SafeRadius;
+
+                if (distance < safeDistance) {
+                    vector.Normalize();
+                    vector *= (safeDistance - distance) / safeDistance;
+                    sum += vector;
+                }
+            }
+
+            if (sum.Length > 1.0f) {
+                sum.Normalize();
+            }
+
+            return sum * SeparationStrength;
         }
 
-        private Vector3 CalcSeparationAccel() { 
-        
+        private Vector3 CalcSeparationAccel(MovingObject boid) {
+
+            Vector3 v = AveragePosition - boid.Position;
+            float distance = v.X * v.X + v.Y * v.Y + v.Z * v.Z;
+            v.Normalize();
+
+            if (distance < FlockRadius) { 
+                v *= distance / FlockRadius;
+            }
+
+            return v * CohesionStrength;
+
+
         }
 
 
